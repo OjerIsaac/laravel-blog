9fdd28f0c9530187cb542a6ad230ff113e7427b4
OjerIsaac <isaacojerumu@gmail.com>
2024-07-01 19:36:29 +0100

    chore: UI cleanup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard > Welcome '. Auth::user()->name) }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,10 +13,6 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
-
-                    <hr>
 
                     @if ($posts->count())
                         @foreach ($posts as $post)
